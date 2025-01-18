87a93951d543de40f7d23d07f02d882fcbe602d0
Mariia Mohylska <mariia.mohylska@gmail.com>
2025-01-17 19:29:22 -0500

    Initial Commit

---
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingBootcampApp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Mariia Mohylska on 1/17/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
