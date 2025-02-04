416916698c42439866511d7444c47ece1e226509
Mariia Mohylska <mariia.mohylska@gmail.com>
2025-02-03 22:56:02 -0500

    Binding properties

---
+++ SwiftfulThinkingBootcamp/BindingBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  BindingBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Mariia Mohylska on 2/3/25.
+//
+
+import SwiftUI
+
+struct BindingBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BindingBootcamp()
+}
